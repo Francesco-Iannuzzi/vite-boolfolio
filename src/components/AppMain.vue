1b3f1cd643ced128e65c7e2de7167fcb67916c43
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-13 18:07:27 +0200

    add methods and button pagination

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -33,7 +33,13 @@ export default {
         },
         getImage(path) {
             return this.api_url + 'storage/' + path;
-        }
+        },
+        prevPage(path) {
+            this.getProjects(path)
+        },
+        nextPage(path) {
+            this.getProjects(path)
+        },
     },
     mounted() {
         const url = this.api_url + this.projects_path
@@ -51,16 +57,35 @@ export default {
             <h1 class="fs-4 text-secondary my-4">Projects</h1>
             <!-- /title page -->
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5 mb-4">
                 <ProjectCard :title="project.title" :author="project.made_by" :description="project.description"
                     :date="project.creation_date" :cover="getImage(project.cover)" :link="project.link"
                     :code="project.code_link" :trace="project.trace" :technologies="project.technologies"
-                    :type="project.type" v-for="project in projects" />
+                    :type="project.type" v-for="project in projects.data" />
                 <!-- /ProjectCard -->
             </div>
             <!-- /row -->
 
+            <nav aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="page-item">
+                        <button class="page-link" aria-label="Previous" v-if="projects.prev_page_url"
+                            @click="prevPage(projects.prev_page_url)">
+                            <span aria-hidden="true">&laquo;</span>
+                        </button>
+                    </li>
+                    <li class="page-item">
+                        <button class="page-link" aria-label="Next" v-if="projects.next_page_url"
+                            @click="nextPage(projects.next_page_url)">
+                            <span aria-hidden="true">&raquo;</span>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /page navigation -->
+
         </div>
+
 
     </section>
 </template>
