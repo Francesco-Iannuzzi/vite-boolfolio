8040698e560ede748f1990c6974610bc9b7b9889
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-14 14:34:02 +0200

    install vue-router + fix bootstrapp main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from '../node_modules/bootstrap/scss/bootstrap.scss'
 
 createApp(App).mount('#app')
