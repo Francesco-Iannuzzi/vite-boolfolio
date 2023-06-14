6e2f85e7ab0681dc41d23fb08d1398323c44d3d1
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-14 14:45:31 +0200

    fix name file router.js and import its in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+
+import { router } from "./router";
+
 import App from './App.vue'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from '../node_modules/bootstrap/scss/bootstrap.scss'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
