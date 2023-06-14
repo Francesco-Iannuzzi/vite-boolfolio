10ebeae78d861274d860c95498e80995cbb875c0
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-14 15:32:13 +0200

    fix router.js routes

--- src/router.js
+++ src/router.js
@@ -5,25 +5,25 @@ import AboutView from './views/AboutView.vue';
 import ContactsView from './views/ContactsView.vue';
 
 const router = createRouter({
-    history: createWebHistory({
-        routes: [
-            {
-                'path': '/',
-                'name': 'home',
-                'component': HomeView
-            },
-            {
-                'path': '/about',
-                'name': 'about',
-                'component': AboutView
-            },
-            {
-                'path': '/contacts',
-                'name': 'contacts',
-                'component': ContactsView
-            },
-        ]
-    })
+    history: createWebHistory(),
+    routes: [
+        {
+            'path': '/',
+            'name': 'home',
+            'component': HomeView
+        },
+        {
+            'path': '/about',
+            'name': 'about',
+            'component': AboutView
+        },
+        {
+            'path': '/contacts',
+            'name': 'contacts',
+            'component': ContactsView
+        },
+    ]
+
 })
 
 export { router }
