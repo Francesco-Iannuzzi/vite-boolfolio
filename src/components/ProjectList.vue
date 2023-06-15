6d46687f20f54d4629df7fbe4a9f898203381a66
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-15 09:34:49 +0200

    add loading cards placeholders

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -54,7 +54,7 @@ export default {
 
         <div class="container py-5">
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5 mb-4">
+            <div v-if="projects && !loading" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5 mb-4">
                 <ProjectCard :title="project.title" :slug="project.slug" :author="project.made_by"
                     :description="project.description" :date="project.creation_date" :cover="getImage(project.cover)"
                     :link="project.link" :code="project.code_link" :trace="project.trace"
@@ -62,6 +62,87 @@ export default {
                 <!-- /ProjectCard -->
             </div>
             <!-- /row -->
+            <div v-else>
+                <div class="row">
+                    <div class="col">
+                        <div class="card" aria-hidden="true">
+                            <img src="http://127.0.0.1:8000/storage/placeholders/26c85b7c0475863bc6e1898b672c48b5.png"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title placeholder-glow">
+                                    <span class="placeholder col-6"></span>
+                                </h5>
+                                <p class="card-text placeholder-glow">
+                                    <span class="placeholder col-7"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-6"></span>
+                                    <span class="placeholder col-8"></span>
+                                </p>
+                                <a href="#" tabindex="-1" class="btn btn-warning disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" aria-hidden="true">
+                            <img src="http://127.0.0.1:8000/storage/placeholders/26c85b7c0475863bc6e1898b672c48b5.png"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title placeholder-glow">
+                                    <span class="placeholder col-6"></span>
+                                </h5>
+                                <p class="card-text placeholder-glow">
+                                    <span class="placeholder col-7"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-6"></span>
+                                    <span class="placeholder col-8"></span>
+                                </p>
+                                <a href="#" tabindex="-1" class="btn btn-warning disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" aria-hidden="true">
+                            <img src="http://127.0.0.1:8000/storage/placeholders/26c85b7c0475863bc6e1898b672c48b5.png"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title placeholder-glow">
+                                    <span class="placeholder col-6"></span>
+                                </h5>
+                                <p class="card-text placeholder-glow">
+                                    <span class="placeholder col-7"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-6"></span>
+                                    <span class="placeholder col-8"></span>
+                                </p>
+                                <a href="#" tabindex="-1" class="btn btn-warning disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" aria-hidden="true">
+                            <img src="http://127.0.0.1:8000/storage/placeholders/26c85b7c0475863bc6e1898b672c48b5.png"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title placeholder-glow">
+                                    <span class="placeholder col-6"></span>
+                                </h5>
+                                <p class="card-text placeholder-glow">
+                                    <span class="placeholder col-7"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-4"></span>
+                                    <span class="placeholder col-6"></span>
+                                    <span class="placeholder col-8"></span>
+                                </p>
+                                <a href="#" tabindex="-1" class="btn btn-warning disabled placeholder col-6"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
             <nav aria-label="Page navigation">
                 <ul class="pagination">
